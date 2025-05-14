5c3a9aaaeacfb4c0e1f10a2762138c8c6035eece
Irina Wipf <irina.wipf@swisstph.ch>
2025-05-14 15:22:00 +0200

    fixed bug when loading test dataset

---
+++ R/data-example_data.R
@@ -0,0 +1,11 @@
+#' Example data for testing
+#'
+#' A dataset containing example user data (made up)
+#'
+#' @format A data frame with 50 rows
+#' \describe{
+#'   \item{var1}{test}
+#'   \item{var2}{test}
+#' }
+#' @source Generated for package testing.
+"example_data"
