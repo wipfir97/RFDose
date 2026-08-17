b63fab036d214976d3db13f839024fb34cccc0a2
nicolz26 <nicolas.loizeau@hotmail.com>
2026-08-17 22:44:21 +0200

    test mobile data done

--- tests/testthat/test-mobiledata.R
+++ tests/testthat/test-mobiledata.R
@@ -142,18 +142,18 @@ cases_mobiledata_dose <- list(
   list(
     input = list(
       tissue           = "brain",
-      duration_low     = 3989,
-      duration_lowmed  = 4917,
-      duration_medhigh = 5651,
+      duration_low     = 3989.318141,
+      duration_lowmed  = 4916.971705,
+      duration_medhigh = 5650.714779,
       duration_high    = 0,
       use_5g           = TRUE,
-      wifi_prop_home   = 0.74,
-      wifi_prop_work   = 0.49,
-      wifi_prop_travel = 0.31,
+      wifi_prop_home   = 0.7378683,
+      wifi_prop_work   = 0.4892182,
+      wifi_prop_travel = 0.3071721,
       urbanicity       = "suburban",
       travel_time      = 1850
     ),
-    output = 146.95
+    output = 146.94667
   ),
   list(
     input = list(
@@ -163,13 +163,65 @@ cases_mobiledata_dose <- list(
       duration_medhigh = 5651,
       duration_high    = 0,
       use_5g           = TRUE,
-      wifi_prop_home   = 0.74,
-      wifi_prop_work   = 0.49,
-      wifi_prop_travel = 0.31,
+      wifi_prop_home   = 0.7378683,
+      wifi_prop_work   = 0.4892182,
+      wifi_prop_travel = 0.3071721,
       urbanicity       = "suburban",
-      travel_time      = 1850
+      travel_time      = 1800
     ),
-    output = 78.69
+    output = 78.68703
+  ), 
+
+  list(
+    input = list(
+      tissue           = "body",
+      duration_low     = 0,
+      duration_lowmed  = 0,
+      duration_medhigh = 0,
+      duration_high    = 0,
+      use_5g           = TRUE,
+      wifi_prop_home   = 0.7378683,
+      wifi_prop_work   = 0.4892182,
+      wifi_prop_travel = 0.3071721,
+      urbanicity       = "suburban",
+      travel_time      = 1800
+    ),
+    output = 0
+  ), 
+
+  list(
+    input = list(
+      tissue           = "body",
+      duration_low     = 0,
+      duration_lowmed  = 0,
+      duration_medhigh = 0,
+      duration_high    = 80000,
+      use_5g           = TRUE,
+      wifi_prop_home   = 0.7378683,
+      wifi_prop_work   = 0.4892182,
+      wifi_prop_travel = 0.3071721,
+      urbanicity       = "suburban",
+      travel_time      = 1800
+    ),
+    output = 4800.352339
+
+  ), 
+
+  list(
+    input = list(
+      tissue           = "brain",
+      duration_low     = 0,
+      duration_lowmed  = 0,
+      duration_medhigh = 0,
+      duration_high    = 80000,
+      use_5g           = TRUE,
+      wifi_prop_home   = 0.7378683,
+      wifi_prop_work   = 0.4892182,
+      wifi_prop_travel = 0.3071721,
+      urbanicity       = "suburban",
+      travel_time      = 1800
+    ),
+    output = 8922.30527
   )
 )
 
